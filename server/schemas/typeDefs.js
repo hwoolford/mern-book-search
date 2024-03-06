9334fa566c692806ca97bb49c72bb7b291109c02
forxtoisate <hwoolford@gmail.com>
2024-03-06 10:20:42 -0600

    Added schema folder and created the index.js, resolvers.js, and typeDefs.js. Filled out the index.js and typeDefs.js

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,40 @@
+import { gql } from 'apollo-server-express';
+
+const typeDefs = gql`
+  type Book {
+    _id: ID!
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
+
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    savedBooks: [Book]!
+    bookCount: Int
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User!
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: Book): User
+    removeBook(bookId: String!): User
+  }
+`;
+
+export default typeDefs;
